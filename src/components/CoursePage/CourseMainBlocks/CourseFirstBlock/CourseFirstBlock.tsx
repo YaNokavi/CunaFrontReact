3d77194a0a512772fdbdff0234e569b4ef0635af
YaNokavi <116379322+YaNokavi@users.noreply.github.com>
2026-06-08 00:47:13 +0300

    refactor: remove isFavorite prop and IsNotFavoriteButton, always render course buttons

--- src/components/CoursePage/CourseMainBlocks/CourseFirstBlock/CourseFirstBlock.tsx
+++ src/components/CoursePage/CourseMainBlocks/CourseFirstBlock/CourseFirstBlock.tsx
@@ -1,18 +1,16 @@
 import CourseWelcomeInfoBlock from "./CourseWelcomeInfo/CourseWelcomeInfoBlock";
-import IsFavoriteButtons from "./Buttons/IsFavoriteButtons";
-import IsNotFavoriteButton from "./Buttons/IsNotFavoriteButton";
+import CourseButtons from "./Buttons/CourseButtons";
 import type { ICourseDetails } from "../../../../../types/CourseTypes/course.types";
 
 interface Props {
   courseData: ICourseDetails;
-  isFavorite: boolean;
 }
 
-export default function CourseFirstBlock({ courseData, isFavorite }: Props) {
+export default function CourseFirstBlock({ courseData }: Props) {
   return (
     <div className="block course-block">
       <CourseWelcomeInfoBlock courseData={courseData} />
-      {isFavorite ? <IsFavoriteButtons /> : <IsNotFavoriteButton />}
+      <CourseButtons />
     </div>
   );
 }
