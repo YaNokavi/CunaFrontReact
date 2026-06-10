87f24250419b733909a1b51ea89abbd0e5b60a42
YaNokavi <116379322+YaNokavi@users.noreply.github.com>
2026-06-10 20:10:37 +0300

    fix: restore main.tsx with App.tsx + add initTelegram()

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./router/router.tsx";
+import App from "./App.tsx";
 import { initTelegram } from "./services/telegram.service.ts";
 import "./index.css";
 
@@ -9,6 +8,6 @@ initTelegram();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
 );
