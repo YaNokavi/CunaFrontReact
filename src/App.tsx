4d0c17f4010ae8983a849c2cf88976201e5299b9
YaNokavi <116379322+YaNokavi@users.noreply.github.com>
2026-06-10 20:12:33 +0300

    fix: add QueryClientProvider to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { router } from "./providers/router.tsx";
 import { TelegramUserContextProvider } from "./context/UserTelegramContext.tsx";
 
+const queryClient = new QueryClient();
+
 export default function App() {
   return (
-    <TelegramUserContextProvider>
-      <RouterProvider router={router} />
-    </TelegramUserContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <TelegramUserContextProvider>
+        <RouterProvider router={router} />
+      </TelegramUserContextProvider>
+    </QueryClientProvider>
   );
 }
