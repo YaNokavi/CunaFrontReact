7b12e00f80da2d9cdf95f5185ec01f843a0214a2
YaNokavi <116379322+YaNokavi@users.noreply.github.com>
2026-06-10 19:59:32 +0300

    feat: add haptic feedback on NavigationBar tab click

--- src/components/layout/NavigationBar/NavigationBar.tsx
+++ src/components/layout/NavigationBar/NavigationBar.tsx
@@ -1,6 +1,7 @@
 import { Link, useLocation } from "react-router-dom";
 import { tabs } from "./tabs.data.tsx";
 import styles from "./styles.module.scss";
+import { hapticMedium } from "../../../services/telegram.service.ts";
 
 export default function NavigationBar() {
   const location = useLocation();
@@ -13,7 +14,13 @@ export default function NavigationBar() {
   return (
     <nav className={styles.tabbar}>
       {tabs.map(({ path, icon, title }) => (
-        <Link key={path} className={getItemClassName(path)} to={path} replace>
+        <Link
+          key={path}
+          className={getItemClassName(path)}
+          to={path}
+          replace
+          onClick={hapticMedium}
+        >
           {icon}
           {title}
         </Link>
